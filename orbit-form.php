3fcf5827f56392b73dd784fc62be4b1e5a0cd644
Steve <anil.stephen66@gmail.com>
2019-05-29 10:57:00 +0530

    updated styles

--- orbit-form.php
+++ orbit-form.php
@@ -6,6 +6,14 @@ Version: 1.0
 Author: Sputznik
 */
 
-add_action('orbit-bundle-loaded', function(){
+
+// THIS CONDITION CHECKS IF THE ORBIT-BUNDLE HAS BEEN LOADED FIRST, IF NOT THEN WAIT FOR IT TO LOAD COMPLETELY
+if( class_exists('ORBIT_BASE') ){
+
   include('class-orbit-fep.php');
-});
+}
+else{
+  add_action('orbit-bundle-loaded', function(){
+    include('class-orbit-fep.php');
+  });
+}
